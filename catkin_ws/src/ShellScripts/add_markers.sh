dc2cd58312d31c7f9d622b5634cf3d7024d71aa6
Christian Rabus <christian@ntw-systems.de>
2019-02-10 19:10:32 +0000

    modelling virtual objects

---
+++ catkin_ws/src/ShellScripts/add_markers.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/World/MyWorld.world" &
+sleep 10
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/World/MyMap.yaml" &
+sleep 10
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; rosrun rviz rviz -d /home/workspace/catkin_ws/src/RvizConfig/navigation_with_markers.rviz" &
+sleep 10
+xterm -e " source /home/workspace/catkin_ws/devel/setup.bash; rosrun add_markers add_markers"
---
+++ catkin_ws/src/add_markers/src/add_markers.cpp
@@ -0,0 +1,89 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
+  ros::Rate r(1);
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  // Set our initial shape type to be a cube
+  uint32_t shape = visualization_msgs::Marker::CUBE;
+
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "cube";
+  marker.id = 0;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
+
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker.action = visualization_msgs::Marker::ADD;
+
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  //Pick up Zone
+  marker.pose.position.x = 4.0;
+  marker.pose.position.y = 6.0;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 0.2;
+  marker.scale.y = 0.2;
+  marker.scale.z = 0.2;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 0.0f;
+  marker.color.g = 1.0f;
+  marker.color.b = 0.0f;
+  marker.color.a = 1.0;
+
+  marker.lifetime = ros::Duration();
+  while (marker_pub.getNumSubscribers() < 1)
+  {
+    if (!ros::ok())
+    {
+      return 0;
+    }
+    ROS_WARN_ONCE("Please create a subscriber to the marker");
+    sleep(1);
+  }
+  
+  // Publish the marker
+  marker_pub.publish(marker);
+
+  // Wait 5 seconds
+  ros::Duration(5.0).sleep(); 
+
+  //Hide the marker
+  marker.color.a = 0.0;
+  ROS_INFO("Hide Marker");
+  marker_pub.publish(marker);
+  
+  ROS_INFO("Marker is transfered to drop off zone");
+  // Wait 5 seconds again
+  ros::Duration(5.0).sleep(); 
+
+  //Set new position drop off zone
+  marker.pose.position.x = -4.0;
+  
+  // Unhide Marker
+  marker.color.a = 1.0;
+
+  //publish it
+  marker_pub.publish(marker); 
+  ROS_INFO("Marker is transfered");
+
+  ros::Duration(5.0).sleep(); 
+  
+}
